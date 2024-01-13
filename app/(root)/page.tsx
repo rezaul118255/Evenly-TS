57ab65722f8c5533c911a51e23eb974a111c462a
rezaul118255 <rezaul118255@gmail.com>
2024-01-13 10:46:32 +0600

    project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+
+      <button className="btn btn-primary">Hello daisyUI!</button>
+    </>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins'
+
+})
+
+export const metadata: Metadata = {
+  title: 'Evenly',
+  description: 'Evenly is a platform of product management',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+  )
+}
